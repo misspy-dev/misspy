af03974ef3d3534975915ab2a28f65a7df030239
sonyakunYT <90973770+sonyakun@users.noreply.github.com>
2023-08-26 22:14:20 +0900

    Update http.py

--- misspy/http.py
+++ misspy/http.py
@@ -2,7 +2,16 @@ import json
 
 import httpx
 
-def request_sync(address, i=None, endpoint="test", jobj: dict={"required": True}, header: dict={"Content-Type": "application/json"}):
+from .exception import HTTPException, ClientException
+
+
+def request_sync(
+    address,
+    i=None,
+    endpoint="test",
+    jobj: dict = {"required": True},
+    header: dict = {"Content-Type": "application/json"},
+):
     """request_sync (internal function)
 
     Args:
@@ -20,13 +29,18 @@ def request_sync(address, i=None, endpoint="test", jobj: dict={"required": True}
         if i is not None:
             jobj["i"] = i
         res = client.post(
-            url, 
-            json=json.dumps(jobj, ensure_ascii=False), 
-            headers=header
+            url, data=json.dumps(jobj, ensure_ascii=False), headers=header
         )
         return res.json()
 
-async def request(address, i=None, endpoint="test", jobj: dict={"required": True}, header: dict={"Content-Type": "application/json"}):
+
+async def request(
+    address,
+    i=None,
+    endpoint="test",
+    jobj: dict = {"required": True},
+    files=None,
+):
     """request (internal function)
 
     Args:
@@ -34,20 +48,42 @@ async def request(address, i=None, endpoint="test", jobj: dict={"required": True
         i (string): user token
         endpoint (string): endpoint (example: notes/create)
         jobj (dict): request params. Do not include the i.
-        header (dict, optional): request header. Defaults to {"Content-Type": "application/json"}.
 
     Returns:
         dict: request result
     """
     async with httpx.AsyncClient() as client:
+        url = address + "/api/" + endpoint
         if i is not None:
             jobj["i"] = i
-        res = await client.post(
-            address + "/api/" + endpoint, json=json.dumps(jobj, ensure_ascii=False), headers=json.dumps(header, ensure_ascii=False)
-        )
-        return res.json()
-    
-async def request_guest(address, endpoint, jobj: dict, header: dict={"Content-Type": "application/json"}):
+        if files is not None:
+            res = await client.post(
+                url,
+                data=jobj,
+                files=files,
+            )
+            try:
+                try:
+                    if json.loads(res.text).get("error").get("kind") is not None:
+                        raise ClientException(
+                            res.json()["error"]["message"]
+                            + "\nid: "
+                            + res.json()["error"]["id"]
+                        )
+                    else:
+                        return res.json()
+                except AttributeError:
+                    return json.loads(res.text)
+            except json.decoder.JSONDecodeError:
+                raise HTTPException(res.status_code)
+        else:
+            res = await client.post(url, data=json.dumps(jobj, ensure_ascii=False))
+            return res.json()
+
+
+async def request_guest(
+    address, endpoint, jobj: dict, header: dict = {"Content-Type": "application/json"}
+):
     """request (internal function)
 
     Args:
@@ -61,6 +97,7 @@ async def request_guest(address, endpoint, jobj: dict, header: dict={"Content-Ty
     """
     async with httpx.AsyncClient() as client:
         res = await client.post(
-            address + "/api/" + endpoint, json=json.dumps(jobj, ensure_ascii=False), headers=json.dumps(header, ensure_ascii=False)
+            address + "/api/" + endpoint,
+            data=json.dumps(jobj, ensure_ascii=False),
         )
         return res.json()
